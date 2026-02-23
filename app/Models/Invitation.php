e05a0a69d39c48d9ff08897ae007b32b909dd385
Abdelilah Isswa <tomhary66@gmail.com>
2026-02-23 14:56:49 +0000

    add the relations to the invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+        protected $fillable = [
+        'colocation_id',
+        'email',
+        'token',
+        'status',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function isPending()
+    {
+        return $this->status === 'pending';
+    }
+
+    public function isAccepted()
+    {
+        return $this->status === 'accepted';
+    }
+
+    public function isRefused()
+    {
+        return $this->status === 'refused';
+    }
 }
