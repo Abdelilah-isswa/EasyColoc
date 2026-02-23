6fa6ea2556ff3654d2dc9afbfe015b93ba3913e0
Abdelilah Isswa <tomhary66@gmail.com>
2026-02-23 14:10:06 +0000

    add the migrations for colocations,memberships,categories,expenses,settlements,invitations

---
+++ database/migrations/2026_02_23_135518_create_expenses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('colocation_id')->constrained('colocations')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('title');
+            $table->decimal('amount',10,2);
+            $table->foreignId('payeur_id')->constrained('users')->onDelete('cascade');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
