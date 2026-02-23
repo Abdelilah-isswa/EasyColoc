54a8cbf792d13da2c00351c593a1218278a9d20d
Abdelilah Isswa <tomhary66@gmail.com>
2026-02-23 14:34:51 +0000

    add the relations to expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+        protected $fillable = [
+        'colocation_id',
+        'category_id',
+        'title',
+        'amount',
+        'payeur_id',
+        'date',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payeur()
+    {
+        return $this->belongsTo(User::class, 'payeur_id');
+    }
 }
