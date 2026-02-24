8c64fd97c357d9cdbc1adea48679b0f5d11e522d
Abdelilah Isswa <tomhary66@gmail.com>
2026-02-24 10:10:32 +0000

    add the create colocation controller

--- app/Http/Controllers/CreateColocationController.php
+++ app/Http/Controllers/CreateColocationController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CreateColocationService;
 use Illuminate\Http\Request;
 
 class CreateColocationController extends Controller
 {
-    //
+    public function __invoke(Request $request, CreateColocationService $service)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255']
+        ]);
+
+        $colocation = $service->handle($data);
+
+        return redirect()->route('colocations.show', $colocation);
+    }
 }
