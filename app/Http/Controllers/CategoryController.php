f37440aa374a3da4c946da1fcbd3a2e0b4052840
Abdelilah Isswa <tomhary66@gmail.com>
2026-02-24 14:33:47 +0000

    add the index method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Colocation;
+use App\Models\Category;
 class CategoryController extends Controller
 {
-    //
+      public function index(Colocation $colocation)
+    {
+        // Only owner can manage
+        if (auth()->id() !== $colocation->owner_id) {
+            abort(403, 'Unauthorized');
+        }
+
+        $categories = $colocation->categories;
+
+        return view('categories.index', compact('colocation', 'categories'));
+    }
+    public function store(Request $request, Colocation $colocation)
+    {
+        // Only owner can manage categories
+      //  $this->authorize('manage', $colocation);
+         if (auth()->id() !== $colocation->owner_id) {
+        abort(403, 'Unauthorized.');
+    }
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $colocation->categories()->create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->back()->with('success', 'Category added.');
+    }
+
+    public function destroy(Colocation $colocation, Category $category)
+    {
+       // $this->authorize('manage', $colocation);
+               if (auth()->id() !== $colocation->owner_id) {
+        abort(403, 'Unauthorized.');
+    }
+        $category->delete();
+
+        return redirect()->back()->with('success', 'Category removed.');
+    }
+
+
+
 }
